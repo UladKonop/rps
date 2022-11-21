4809e4e44030d0a51379bbec2398e11bca409d9c
uladkonop <vladknpljov@gmail.com>
2022-11-21 20:24:25 +0300

    add rps services

---
+++ app/services/rps_service.rb
@@ -0,0 +1,46 @@
+class RpsService
+  attr_writer :rand_seed
+
+  def initialize(guess:)
+    @guess = guess
+  end
+
+  def call
+    if rule_engine[computer_guess.to_sym].include? @guess
+      'You lost!'
+    elsif rule_engine[@guess.to_sym].include? computer_guess
+      'You win!'
+    else
+      'Tie'
+    end
+  end
+
+  private
+
+  def randomization_algorithm
+    rand 2342343
+  end
+
+  def computer_guess
+    @computer_guess ||= api_call_computer_guess || offline_computer_guess
+  end
+
+  def api_call_computer_guess
+    ::RpsGuessApiService.call
+  end
+
+  def offline_computer_guess
+    srand (@rand_seed || randomization_algorithm)
+    computer_guesses = %w{rock paper scissors}
+    computer_guesses.sample
+  end
+
+
+  def rule_engine
+    {
+      'rock': ['scissors'],
+      'paper': ['rock'],
+      'scissors': ['paper']
+    }
+  end
+end
