57d1d799390573fa88f73d6e58a1945506eb57f4
uladkonop <vladknpljov@gmail.com>
2022-11-22 15:20:28 +0300

    split modal for two parts

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,10 +1,13 @@
 $(document).ready(() => {
   try {
     const SECONDS_TO_DISPLAY_MODAL = 1.5
+    const modal_container = document.getElementById('modal-container');
     const modal = document.getElementById('modal');
+    const modal_first_part = document.getElementById('first-part');
+    const modal_second_part = document.getElementById('second-part');
     const rps_buttons = document.getElementsByClassName('button')
 
-    // save images sources to display user's choose on modal
+    // save images sources to display user's choose on modal_container
     const local_img_srcs = {}
     for (let i = 0; i < rps_buttons.length; i++) {
       local_img_srcs[rps_buttons[i].getAttribute('value')] = rps_buttons[i].firstElementChild.src
@@ -12,8 +15,8 @@ $(document).ready(() => {
 
     // add handler on each button
     for (let i = 0; i < rps_buttons.length; i++) {
-      rps_buttons[i].onclick = async function () {
-        modal.style.display = 'block';
+      rps_buttons[i].onclick = function () {
+        modal_container.style.display = 'block';
 
         let img = document.createElement("img");
         img.src = local_img_srcs[rps_buttons[i].getAttribute('value')];
@@ -28,7 +31,11 @@ $(document).ready(() => {
             data: { guess: rps_buttons[i].getAttribute('value') },
           }).done((data) => {
 
-              modal.style.display = "none";
+              modal.style.width = "492px";
+              modal.style.height = "591px";
+              modal_first_part.style.display = "none";
+              modal_second_part.style.display = "block";
+
               let users_bet_div = document.getElementById('users_bet');
               let user_bet_img = document.getElementById('users_bet_img');
               users_bet_div.removeChild(user_bet_img);
@@ -42,10 +49,15 @@ $(document).ready(() => {
     // logic for the close button
     const close_btn = document.getElementById('close');
     close_btn.onclick = function() {
-      modal.style.display = "none";
+      modal.style.width = "879px";
+      modal.style.height = "492px";
+      modal_container.style.display = "none";
+      modal_first_part.style.display = "block";
+      modal_second_part.style.display = "none";
       let users_bet_div = document.getElementById('users_bet');
       let user_bet_img = document.getElementById('users_bet_img');
       users_bet_div.removeChild(user_bet_img);
+
     }
   } catch (error) {
     console.error(error);
