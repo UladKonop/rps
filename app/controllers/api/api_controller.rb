03dee122eda3918e42f5552e28b11f84052e21fa
uladkonop <vladknpljov@gmail.com>
2022-11-29 21:02:10 +0300

    add params validation, response statuses

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,5 +1,12 @@
 module Api
   class ApiController < ActionController::Base
     protect_from_forgery with: :null_session
+
+    rescue_from RailsParam::InvalidParameterError, with: :invalid_parameter_error
+    def invalid_parameter_error(exception)
+      render json: {
+        error: exception.message
+      }, status: :unprocessable_entity
+    end
   end
 end
--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,7 +1,14 @@
 module Api
   class GamesController < ApiController
     def create
-      render json: { result: RpsService.new(guess: params[:guess]).call}
+      param! :guess, String, in: %w(rock paper scissors)
+
+      result = RpsService.new(guess: params[:guess]).call
+      if result
+        render json: { result: result }, status: :ok
+      else
+        render json: { result: 'error' }, status: :unprocessable_entity
+      end
     end
   end
 end
