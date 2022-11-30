ebe52af0d650c53941fcf8493404e3ae4585e1ce
uladkonop <vladknpljov@gmail.com>
2022-11-30 12:16:49 +0300

    refactor

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,23 +1,32 @@
 module Api
   class GamesController < ApiController
+    before_action :find_stats_instance, only: :create
     def create
       param! :guess, String, in: %w(rock paper scissors)
-      @stats = Stats.first
 
-      result = RpsService.new(guess: params[:guess]).call
-      if result
-        @stats.update!(counter: @stats.counter + 1)
-        if result[:message] == 'You win!'
-          @stats.update!(wins: @stats.wins + 1)
-        elsif result[:message] == 'You lost!'
-          @stats.update!(loses: @stats.loses + 1)
-        end
-        @percentage = @stats.wins /  @stats.counter.to_f * 100
-
-        render json: { result: result, stats: @stats, percentage: @percentage}, status: :ok
+      @result = RpsService.new(guess: params[:guess]).call
+      if @result
+        update_statistics!
+        render json: { result: @result, stats: @stats }, status: :ok
       else
         render json: { result: 'error' }, status: :unprocessable_entity
       end
+    end
+
+    private
+
+    def update_statistics!
+      @stats.update!(counter: @stats.counter + 1)
+      if @result[:message] == 'You win!'
+        @stats.update!(wins: @stats.wins + 1)
+      elsif @result[:message] == 'You lost!'
+        @stats.update!(loses: @stats.loses + 1)
+      end
+      @stats.update!(percentage: (@stats.wins / @stats.counter.to_f * 100))
+    end
+
+    def find_stats_instance
+      @stats = Stats.first
     end
   end
 end
